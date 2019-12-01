d3cc9ea2a709a696d4a0627b4cd1333a7f4e70c0
Jasmin Huang <wow92716@gmail.com>
2019-12-02 01:35:54 +0800

    Updates

---
+++ precache-manifest.a3cc6cc2b8a74a6884c46cedb5e6c7ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6061b8e22d0cb35b2727f35305720bd",
+    "url": "/music-app/index.html"
+  },
+  {
+    "revision": "84fd38b2552bd8a64d62",
+    "url": "/music-app/static/css/main.65fd8923.chunk.css"
+  },
+  {
+    "revision": "ce8368da56972567324b",
+    "url": "/music-app/static/js/2.88ffffb9.chunk.js"
+  },
+  {
+    "revision": "84fd38b2552bd8a64d62",
+    "url": "/music-app/static/js/main.f8df7d22.chunk.js"
+  },
+  {
+    "revision": "6fcc9131d98077c6d37e",
+    "url": "/music-app/static/js/runtime-main.2a258812.js"
+  }
+]);
